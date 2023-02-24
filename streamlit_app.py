e214eb8ea8c342c3c02b31ea858bc23803374304
Coding Professor <82854291+coding-professor@users.noreply.github.com>
2023-02-24 11:10:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation App')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vTlu3-j10StHZe5LxOTp3aRdnBZ1-sqlGXm-3ulVZzTq6qSqFO4NNsil-_EWgoDv9OcofVpPhOClLpk/embed?start=false&loop=false&delayms=3000", height=565)
+components.iframe("https://docs.google.com/presentation/d/1l76sm8hUm9unjYjBrlmZt286NRu6zDVdgMv7M3IDjVM/edit?usp=sharing", height=565)
