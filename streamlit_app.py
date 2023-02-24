0644a1210e4ec1321a9531afe5c6529fb1e293f8
Coding Professor <82854291+coding-professor@users.noreply.github.com>
2023-02-24 11:15:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation Deck')
-components.iframe("https://docs.google.com/presentation/d/1l76sm8hUm9unjYjBrlmZt286NRu6zDVdgMv7M3IDjVM/edit?usp=sharing", height=565)
-
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSaBx4udmh8hYvD7r8iQPMGpdrOCYPjKTo09pezMX5dyVNHJ0CBu2n8oEX-7DR-j9hGRvB-EBeI9Yaz/embed?start=false&loop=false&delayms=3000", height=565)
